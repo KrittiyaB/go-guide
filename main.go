b018343c960f546e92b300c56892f81f25349b0d
Krittiya B <krittiya.bo@kbtg.tech>
2022-03-25 22:25:42 +0700

    shuffle card with fake random

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spades"
 	// cards := newDeck()
@@ -9,30 +7,16 @@ func main() {
 	// hand, remainingCards := deal(cards, 5)
 	// hand.print()
 	// remainingCards.print()
+
 	// cards := newDeck()
 	// fmt.Println(cards.toString())
 	// cards.saveToFile("my_cards")
-	cards2 := newDeckFromFile("my_cards1")
-	fmt.Println(cards2)
+
+	// cards2 := newDeckFromFile("my_cards")
+	// fmt.Println(cards2)
+
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
 
 }
-
-// import (
-// 	"fmt"
-// 	"os"
-// 	"path/filepath"
-// )
-
-// func main() {
-// 	err := filepath.Walk("./UAT", func(path string, info os.FileInfo, err error) error {
-// 		if err != nil {
-// 			fmt.Println(err)
-// 			return err
-// 		}
-// 		fmt.Printf("dir: %v: name: %s\n", info.IsDir(), path)
-// 		return nil
-// 	})
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// }
